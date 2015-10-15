06bf3f3a1146dc5b553ec87adbe869f38f8ba5bd
UWPanda <uwp_anda@outlook.com>
2015-10-15 16:03:01 +0200

    initial commit

---
+++ TabbedPivot/TabbedPivot/PivotHeader.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace TabbedPivot
+{
+    public sealed partial class PivotHeader : UserControl
+    {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(PivotHeader), null);
+
+        public string Text
+        {
+            get { return GetValue(TextProperty) as string; }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(string), typeof(PivotHeader), null);
+
+        public string Icon
+        {
+            get { return GetValue(IconProperty) as string; }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public PivotHeader()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
